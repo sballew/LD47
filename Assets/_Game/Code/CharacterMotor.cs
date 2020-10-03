8bcf3fbe856dfd255c1fd7fbc7e7f39cdd1ae7fd
Sean Ballew <sballew7@gmail.com>
2020-10-02 20:42:23 -0700

    More stuff

---
+++ Assets/_Game/Code/CharacterMotor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwinPixels.LD47
+{
+    public class CharacterMotor : MonoBehaviour
+    {
+        
+    }
+}
